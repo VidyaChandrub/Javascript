f0a4d5f556d7c14a9db8364d6c0de85b7c3b596c
VidyaChandrub <vidyac266@gmail.com>
2022-11-13 21:27:20 +0530

    upload

---
+++ FatArrow/fat.js
@@ -0,0 +1,30 @@
+/* let a = 10;
+let b = function (msg){
+
+    return "hello"+msg
+
+}
+let res = b("Gm");
+console.log(res); */
+
+/* let add = (msg) => {
+    return "hello "+ msg
+}
+let output = add("Gm");
+console.log(output); */
+
+let sum = (a,b) => {
+    return a+b;
+}
+let op = sum(45,35);
+console.log(op);
+
+let c = sum(34);
+console.log(c);
+
+let d = sum("heelo", "gm");
+console.log(d);
+
+let e = sum("hello",64);
+console.log(e);
+
---
+++ FatArrow/one.js
@@ -0,0 +1,13 @@
+/* function sum(a,b){
+
+    //console.log(a+b);
+    return a+b;
+}
+let res = sum(3,4);
+console.log(res); */
+
+function cal_Age(age){
+    return (2022 - age)
+}
+let present_age = cal_Age(2001);
+console.log(present_age);
