7dc9f88f3314bdf2090f045ac5a238965e8122fe
VidyaChandrub <vidyac266@gmail.com>
2022-11-15 11:14:55 +0530

    this

--- this.js
+++ this.js
@@ -1,3 +1,19 @@
 class Account{
-    
+    min_Bal = 500;
+    amount;
+    open_Act(){
+        console.log("Account opened successfully..");
+    }
+
+    dep_Amt(Amount){
+       return  this.Amount;
+    }
 }
+
+let a1 = new Account();
+console.log(a1);
+a1.open_Act();
+//a1.dep_Amt();
+a1.dep_Amt(5000);
+console.log(a1);
+
