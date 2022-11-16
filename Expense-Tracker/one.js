57c5e050b49ffb3a8fa1675a9baaa45036632c6b
VidyaChandrub <vidyac266@gmail.com>
2022-11-16 21:24:33 +0530

    file

---
+++ Expense-Tracker/one.js
@@ -0,0 +1,14 @@
+let amount=document.getElementById('amount')
+let income=document.getElementById('income')
+let expense=document.getElementById('expense')
+let list=document.getElementById('list')
+let form=document.getElementById('form')
+let text1=document.getElementById('text1')
+let amt=document.getElementById('amt')
+
+let dummyTransaction = [{id:1, text:'Flower', amount:-29},
+                        {id:2, text:'Salary', amount:2900},
+                        {id:1, text:'book', amount:-20},
+                        {id:1, text:'camera', amount:109},]
+
+let transactions = dummyTransaction;
