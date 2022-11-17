b401332cfe284f70b5e438544b350219ebb1db10
VidyaChandrub <vidyac266@gmail.com>
2022-11-17 21:48:04 +0530

    expense

--- Expense-Tracker/one.js
+++ Expense-Tracker/one.js
@@ -1,14 +1,103 @@
-let amount=document.getElementById('amount')
-let income=document.getElementById('income')
-let expense=document.getElementById('expense')
-let list=document.getElementById('list')
-let form=document.getElementById('form')
-let text1=document.getElementById('text1')
-let amt=document.getElementById('amt')
+let amount_bal = document.getElementById('amount')
+let inc = document.getElementById('income')
+let exp = document.getElementById('expense')
+let list_history = document.getElementById('list')
+let add_income = document.getElementById('Add-Income')
+let add_expense = document.getAnimations('Add-Expense')
+let namein = document.getElementById('text1')
+let amtin = document.getElementById('amt')
 
-let dummyTransaction = [{id:1, text:'Flower', amount:-29},
-                        {id:2, text:'Salary', amount:2900},
-                        {id:1, text:'book', amount:-20},
-                        {id:1, text:'camera', amount:109},]
+let transaction_amt = {
+                        amount: 100,
+                        income: 200,
+                        expense: 100,
 
-let transactions = dummyTransaction;
+                       transcatins: [{text:'Flower', amt:829, type:'income'},
+                        { text:'Salary', amt:2900, type:'expense'},
+                        { text:'book', amt:920, type:'income'},
+                        { text:'camera', amt:109, type:'income'},
+                    ]
+
+                    }
+function init(){
+    updateState();
+    initEvents();
+    render();
+}
+
+function initEvents(){
+    add_income.addEventListener('click', onAddIncomeClick);
+    add_expense.addEventListener('click', onAddExpenseClick);
+ 
+}
+
+function onAddIncomeClick(){
+    
+    var transaction = { text: namein.value,
+         amt: parseInt(amtin.value), type:'income'
+        };
+
+        transaction_amt.transcatins.push(transaction);
+
+        updateState();
+}
+
+function onAddExpenseClick(){
+    console.log('expense');
+    
+}
+function updateState(){
+    var amount = 0,
+    income = 0,
+    expense = 0,
+    item;
+    for(var i=0; i<transaction_amt.transcatins.length;i++){
+        item = transaction_amt.transcatins[i];
+
+        if(item.type === 'income'){
+            income +=item.amt;
+        }else if(item.type === 'expense'){
+            expense +=item.amt;
+        }
+    }
+    amount = income - expense;
+    
+    transaction_amt.amount = amount;
+    transaction_amt.income = income;
+    transaction_amt.expense = expense;
+
+}
+function render(){
+    amount_bal.innerHTML = `${transaction_amt.amount}`;
+    inc.innerHTML = `${transaction_amt.income}`;
+    exp.innerHTML = `${transaction_amt.expense}`;
+    
+    var list_items,list_cont,list_amt,item,btnel;
+
+    for(var i=0; i<transaction_amt.transcatins.length; i++)
+    {   item = transaction_amt.transcatins[i];
+        list_items = document.createElement('li');
+        list_items.append(item.text);
+
+        list_history.appendChild(list_items);
+
+        list_cont = document.createElement('div');
+        list_amt = document.createElement('span');
+        if(item.type === 'income'){
+            list_amt.classList.add('income-amt');
+        }else if(item.type === 'expense'){
+            list_amt.classList.add('expense-amt');
+        }
+        list_amt.innerHTML = `${item.amt}`
+        list_cont.appendChild(list_amt);
+
+        btnel = document.createElement('button');
+        btnel.innerHTML = 'X';
+        list_cont.appendChild(btnel);
+
+        list_items.appendChild(list_cont);
+
+    }
+    
+}
+init();
