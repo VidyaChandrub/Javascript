807739bd94a4759d28d78c55566229097f60999d
VidyaChandrub <vidyac266@gmail.com>
2022-11-24 19:31:08 +0530

    parse

---
+++ Assignments/math.js
@@ -0,0 +1,11 @@
+/* function randomWholeNum(max,min) {
+
+    
+  
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+
+  console.log(randomWholeNum()); */
+//   const a = parseInt("007");
+const a = parseInt("11", 2);
+console.log(a);
