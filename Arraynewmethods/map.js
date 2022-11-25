adce28e1114e9d0fa9c428b2e6ec8ffdaf5db037
VidyaChandrub <vidyac266@gmail.com>
2022-11-25 19:28:01 +0530

    okie

---
+++ Arraynewmethods/map.js
@@ -0,0 +1,29 @@
+/* function calculate(a, b) {
+    const add = a + b;
+    const subtract = a - b;
+    const multiply = a * b;
+    const divide = a / b;
+  
+    return [add, subtract, multiply, divide];
+  }
+  
+  const [add, subtract, multiply, divide] = calculate(4, 7);
+  console.log(calculate(4, 7));
+ */
+
+  const myVehicle = {
+    brand: 'Ford',
+    model: 'Mustang',
+    color: 'red'
+  }
+  
+  const updateMyVehicle = {
+    type: 'car',
+    year: 2021, 
+    color: 'yellow'
+  }
+  
+  const myUpdatedVehicle = {...myVehicle, ...updateMyVehicle}
+  
+  //Check the result object in the console:
+  console.log(myUpdatedVehicle);
